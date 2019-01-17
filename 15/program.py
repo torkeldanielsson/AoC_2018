ce436b3dfb616a3335085f320d255e3bf10460c1
Torkel <torkel@voysys.se>
2019-01-17 23:58:57 +0100

    wip

--- 15/program.py
+++ 15/program.py
@@ -1,21 +1,47 @@
 import sys
-import copy
-from datetime import datetime
-import operator
 
-f = open("test_1.txt", "r")
+f = open("test_2.txt", "r")
 lines = f.readlines()
 f.close()
 
-def print_map(map, units):
-    for y, row in enumerate(map):
+class Unit:
+    isElf = True
+    hp = 200
+    ap = 3
+    pos = 0 + 0j
+
+    def __lt__(self, other):
+        if self.pos.imag == other.pos.imag:
+            return self.pos.real < other.pos.real
+        return self.pos.imag < other.pos.imag
+
+height = len(lines)
+width = len(lines[0].strip("\n"))
+
+free_space = []
+units = []
+neighbors = [-1j, -1, 1, 1j]
+
+for y, row in enumerate(lines):
+    for x, c in enumerate(row.strip("\n")):
+        loc = x + y*1j
+        if c != "#":
+            free_space.append(loc)
+        if c in ["G", "E"]:
+            unit = Unit()
+            unit.isElf = c == "E"
+            unit.pos = loc
+            units.append(unit)
+
+def print_map():
+    for y in range(height):
         row_str = ""
         unit_str = " "
-        for x, loc in enumerate(row):
-
+        for x in range(width):
+            loc = x + y*1j
             isUnit = False
             for unit in units:
-                if unit.pos == (x, y):
+                if unit.pos == loc:
                     isUnit = True
                     unit_desc = "G"
                     if unit.isElf:
@@ -24,86 +50,127 @@ def print_map(map, units):
                     unit_str += unit_desc + str(unit.hp) + ", "
 
             if not isUnit:
-                if loc:
+                if loc not in free_space:
                     row_str += "#"
                 else:
                     row_str += " "
         print(row_str + unit_str)
 
-class Unit:
-    isElf = True
-    hp = 200
-    ap = 3
-    pos = (0, 0)
+def make_move_map(pos, units):
+    move_map = {}
+    for y in range(height):
+        for x in range(width):
+            move_map[x + y*1j] = -1
 
-    def __lt__(self, other):
-        if self.pos[1] == other.pos[1]:
-            return self.pos[0] < other.pos[0]
-        return self.pos[1] < other.pos[1]
-
-map = []
-units = []
-
-for y, line in enumerate(lines):
-    row = []
-    for x, c in enumerate(line.strip("\n")):
-        row.append(c == "#")
-        if c in ["G", "E"]:
-            unit = Unit()
-            unit.isElf = c == "E"
-            unit.pos = (x, y)
-            units.append(unit)
-    map.append(row)
-
-neighbors = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
-
-def make_move_map(map, unit, units):
-    move_map = []
-    for r in map:
-        move_map.append([-1] * len(r))
-
-    next_candidates = [unit.pos]
+    next_candidates = set()
+    next_candidates.add(pos)
     next_dist = 0
     done = False
     while not done:
-        next_next = []
+        next_next = set()
         done = True
         for nc in next_candidates:
-            if 0 <= nc[1] and nc[1] < len(map) and 0 <= nc[0] and nc[0] < len(map[nc[1]]):
-                if not map[nc[1]][nc[0]] and (move_map[nc[1]][nc[0]] == -1 or move_map[nc[1]][nc[0]] > next_dist):
-                    has_unit = False
-                    for ounit in units:
-                        if nc == ounit.pos:
-                            has_unit = True
+            if nc in free_space and (move_map[nc] == -1 or move_map[nc] > next_dist):
+                has_unit = False
+                for unit in units:
                     if nc == unit.pos:
-                        has_unit = False
-                    if not has_unit:
-                        done = False
-                        move_map[nc[1]][nc[0]] = next_dist
-                        for neighbor in neighbors:
-                            next_next.append((nc[0] + neighbor[0], nc[1] + neighbor[1]))
+                        has_unit = True
+                if nc == pos:
+                    has_unit = False
+                if not has_unit:
+                    done = False
+                    move_map[nc] = next_dist
+                    for neighbor in neighbors:
+                        next_next.add(nc + neighbor)
         next_candidates = next_next
         next_dist += 1
     return move_map
 
 def print_move_map(move_map):
-    for row in move_map:
+    for y in range(height):
         row_str = ""
-        for loc in row:
-            if loc == -1:
+        for x in range(width):
+            loc = x + y*1j
+            if not loc in free_space:
+                row_str += "#"
+            elif move_map[loc] == -1:
                 row_str += " "
             else:
-                row_str += str(loc)
+                row_str += str(move_map[loc])
+            if not loc in free_space and move_map[loc] != -1:
+                print("error")
         print(row_str)
 
-for _ in range(4):
+bignum = 9999999999
+
+print("Initially:")
+print_map()
+
+for round in range(24):
 
     # sort
     units.sort()
 
     # move and fight
-    for unit in units:
-        move_map = make_move_map(map, unit, units)
-        print_move_map(move_map)
 
-print_map(map, units)
+    for unit in units:
+
+        can_attack = False
+        for n in neighbors:
+            for i, enemy in enumerate(units):
+                if enemy.isElf != unit.isElf:
+                    if enemy.pos == unit.pos + n:
+                        can_attack = True
+
+        print(unit.pos, can_attack, unit.isElf)
+
+        if not can_attack:
+            move_map = make_move_map(unit.pos, units)
+
+            best_target = 0
+            best_target_score = bignum
+
+            for enemy in units:
+                if enemy.isElf != unit.isElf:
+                    for n in neighbors:
+                        neighbor = enemy.pos + n
+                        if neighbor in move_map and move_map[neighbor] != -1 and move_map[neighbor] < best_target_score:
+                            best_target = neighbor
+                            best_target_score = move_map[neighbor]
+
+            if best_target_score != bignum:
+
+                target_move_map = make_move_map(best_target, units)
+
+                best_next_move = 0
+                best_next_move_score = bignum
+
+                for n in neighbors:
+                    neighbor = unit.pos + n
+                    if (neighbor in target_move_map) and (target_move_map[neighbor] != -1) and (target_move_map[neighbor] < best_next_move_score):
+                        best_next_move = neighbor
+                        best_next_move_score = target_move_map[neighbor]
+                
+                if best_next_move != 0:
+                    unit.pos = best_next_move
+
+        # attack 
+
+        attack = False
+        best_target_i = 0
+        best_hp = bignum
+        for n in neighbors:
+            for i, enemy in enumerate(units):
+                if enemy.isElf != unit.isElf:
+                    if enemy.pos == unit.pos + n:
+                        if enemy.hp < best_hp:
+                            attack = True
+                            best_target_i = i
+                            best_hp = enemy.hp
+        if attack:
+            units[best_target_i].hp -= unit.ap
+            if units[best_target_i].hp <= 0:
+                del units[best_target_i]
+
+    print("\nAfter", round+1, "rounds:")
+    print_map()
